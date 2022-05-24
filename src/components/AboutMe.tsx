55302903c686097affe9f75ce8f545bb1924d949
DonquixoteSabo <mixboksu@gmail.com>
2022-05-24 20:19:46 +0200

    chore: Make about-me section responsive

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,20 +2,32 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <section className="flex flex-col-reverse">
-      <div>
+    <section className="flex flex-col-reverse items-center px-12 lg:flex-row lg:justify-between lg:py-24">
+      <div className="max-w-lg">
         <header>
-          <h1 className="text-blue-500">A Simple Bookmark Manager</h1>
+          <h1 className="my-4 text-center text-4xl font-medium text-blue-700 lg:text-left">
+            A Simple Bookmark Manager
+          </h1>
         </header>
-        <p>
+        <p className="mt-4 text-center text-blue-600 lg:my-6 lg:text-left">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sited load instantly. Try it for free.
         </p>
-        <button>Get it on Firefox</button>
+        <div className="flex flex-col items-center text-center sm:flex-row sm:justify-evenly lg:justify-start lg:text-left">
+          <button className="my-4 rounded bg-blue-500 px-4 py-3 font-medium text-white shadow-md lg:mr-8">
+            Get it on Chrome
+          </button>
+          <button className="rounded px-4 py-3 font-medium text-blue-700 shadow-md shadow-slate-300">
+            Get it on Firefox
+          </button>
+        </div>
       </div>
-      <div>
-        <div className="" />
-        <img src="/images/illustration-hero.svg" alt="App preview" />
+      <div className="max-w-lg">
+        <img
+          src="/images/illustration-hero.svg"
+          alt="App preview"
+          className=""
+        />
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      blue: {
-        500: 'hsl(231, 69%, 60%)',
-        600: 'hsl(229, 8%, 60%)',
-        700: 'hsl(229, 31%, 21%)',
+    extend: {
+      colors: {
+        blue: {
+          500: 'hsl(231, 69%, 60%)',
+          600: 'hsl(229, 8%, 60%)',
+          700: 'hsl(229, 31%, 21%)',
+        },
+        red: 'hsl(0, 94%, 66%)',
       },
-      red: 'hsl(0, 94%, 66%)',
     },
   },
   plugins: [],
