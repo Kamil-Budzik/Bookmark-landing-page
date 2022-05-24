20c0677b8541170ef96a0283942d3f534f0353c7
DonquixoteSabo <mixboksu@gmail.com>
2022-05-24 19:11:00 +0200

    build: Add challenge files
    styles, images, templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: [],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
