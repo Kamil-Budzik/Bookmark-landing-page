30aaa44cc642783c5bfe2c038d9030356e21faef
DonquixoteSabo <mixboksu@gmail.com>
2022-05-24 19:28:55 +0200

    build: Move images to public folder, add font family

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,27 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import AboutMe from '../components/AboutMe'
+
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Bookmark</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <main>
-        <div className="flex items-center justify-center">
-          <h1>STARTER TEMPLATE</h1>
-        </div>
+        <AboutMe />
       </main>
     </div>
   )
