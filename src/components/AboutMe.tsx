fdf36d97834eeaa9afaa56cbaf19e8c259634834
DonquixoteSabo <mixboksu@gmail.com>
2022-05-24 19:29:10 +0200

    feat: Add HTML structure

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <section className="">
+      <div>
+        <header>
+          <h1>A Simple Bookmark Manager</h1>
+        </header>
+        <p>
+          A clean and simple interface to organize your favourite websites. Open
+          a new browser tab and see your sited load instantly. Try it for free.
+        </p>
+        <button>Get it on Firefox</button>
+      </div>
+      <div>
+        <img src="/images/illustration-hero.svg" alt="App preview" />
+      </div>
+    </section>
+  )
+}
+
+export default AboutMe
