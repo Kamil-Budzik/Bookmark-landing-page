1f41190b61adb12b65c862a1eded9fe172dc371b
DonquixoteSabo <mixboksu@gmail.com>
2022-05-25 12:14:24 +0200

    chore: Add active state animation

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -5,24 +5,22 @@ const AboutMe = () => {
     <section className="flex flex-col-reverse items-center px-12 lg:flex-row lg:justify-between lg:py-24">
       <div className="max-w-lg">
         <header>
-          <h1 className="my-4 text-center text-4xl font-medium text-blue-700 lg:text-left">
+          <h1 className="my-4 max-w-md text-center text-4xl font-medium text-blue-700 lg:text-left">
             A Simple Bookmark Manager
           </h1>
         </header>
-        <p className="mt-4 text-center text-blue-600 lg:my-6 lg:text-left">
+        <p className="mt-4 max-w-md text-center text-blue-600 lg:my-6 lg:text-left">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sited load instantly. Try it for free.
         </p>
         <div className="flex flex-col items-center text-center sm:flex-row sm:justify-evenly lg:justify-start lg:text-left">
-          <button className="my-4 rounded bg-blue-500 px-4 py-3 font-medium text-white shadow-md lg:mr-8">
+          <button className="primary-button my-4 lg:mr-8">
             Get it on Chrome
           </button>
-          <button className="rounded px-4 py-3 font-medium text-blue-700 shadow-md shadow-slate-300">
-            Get it on Firefox
-          </button>
+          <button className="secondary-button">Get it on Firefox</button>
         </div>
       </div>
-      <div className="max-w-lg">
+      <div className="max-w-xl">
         <img
           src="/images/illustration-hero.svg"
           alt="App preview"
