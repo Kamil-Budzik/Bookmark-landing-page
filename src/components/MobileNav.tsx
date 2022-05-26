d7c8b37fbb52b766988fba853f5bee5aaa9ad5be
DonquixoteSabo <mixboksu@gmail.com>
2022-05-26 15:26:16 +0200

    chore: Add Nav-list mobile styles

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -7,21 +7,35 @@ interface Props {
 const MobileNav = ({ closeNav }: Props) => {
   return (
     <nav className="absolute top-0 bottom-0 left-0 right-0 bg-blue-700 opacity-95">
-      <div>
-        <header className="flex items-center justify-between px-8 py-12">
+      <div className="flex items-center justify-between px-8 py-12">
+        <header>
           <img
             className="w-44 fill-white "
             src="/images/logo-dark.svg"
             alt="Bookmark logo"
           />
-          <img
-            src="/images/icon-close.svg"
-            alt="Close nav"
-            className="w-8 cursor-pointer fill-white"
-            onClick={closeNav}
-          />
         </header>
+        <img
+          src="/images/icon-close.svg"
+          alt="Close nav"
+          className="w-8 cursor-pointer fill-white"
+          onClick={closeNav}
+        />
       </div>
+      <ul className="mx-8 flex h-2/5 flex-col items-center divide-y divide-slate-700 ">
+        <li className="w-full cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+          Features
+        </li>
+        <li className="w-full cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+          Pricing
+        </li>
+        <li className="w-full  cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+          Contact
+        </li>
+        <li className="w-full   cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+          <button>Login</button>
+        </li>
+      </ul>
     </nav>
   )
 }
