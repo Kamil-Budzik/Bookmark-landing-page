afe615833fbbb75066cf75596cbd73a273a50e9a
DonquixoteSabo <mixboksu@gmail.com>
2022-05-26 15:12:09 +0200

    chore: Add closeNav feature

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,35 +1,46 @@
 import React, { useState } from 'react'
 
+import MobileNav from './MobileNav'
+
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false)
 
+  const closeNav = () => setIsOpen(false)
+
   return (
-    <nav className="flex items-center justify-between px-12 pb-12 pt-6">
+    <nav className="flex items-center justify-between px-8 py-12">
       <img
         src="/images/logo-bookmark.svg"
         alt="Bookmark logo"
-        className="sm:w-44"
+        className={`sm:w-44  ${isOpen ? 'hidden' : 'visible'}`}
       />
 
       <img
         src="/images/icon-hamburger.svg"
-        alt="open nav"
-        className="w-8 sm:hidden"
+        alt={isOpen ? 'close navigation' : 'open navigation'}
+        className={`w-8 cursor-pointer  sm:hidden ${
+          isOpen ? 'hidden' : 'visible'
+        }`}
+        onClick={() => setIsOpen(true)}
       />
-      <ul className="hidden items-center sm:flex">
-        <li className="mx-2  cursor-pointer transition hover:text-red md:mx-4">
-          Features
-        </li>
-        <li className="mx-2  cursor-pointer transition hover:text-red md:mx-4">
-          Pricing
-        </li>
-        <li className="mx-2  cursor-pointer transition hover:text-red md:mx-4">
-          Contact
-        </li>
-        <li className="mx-2 md:mx-4">
-          <button className="red-button">Login</button>
-        </li>
-      </ul>
+      {isOpen ? (
+        <MobileNav closeNav={closeNav} />
+      ) : (
+        <ul className="hidden items-center sm:flex">
+          <li className="mx-2  cursor-pointer transition hover:text-red md:mx-4">
+            Features
+          </li>
+          <li className="mx-2  cursor-pointer transition hover:text-red md:mx-4">
+            Pricing
+          </li>
+          <li className="mx-2  cursor-pointer transition hover:text-red md:mx-4">
+            Contact
+          </li>
+          <li className="mx-2 md:mx-4">
+            <button className="red-button">Login</button>
+          </li>
+        </ul>
+      )}
     </nav>
   )
 }
