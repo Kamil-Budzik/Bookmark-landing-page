18f753febed31385868811cda104492e842d216c
DonquixoteSabo <mixboksu@gmail.com>
2022-05-26 15:36:43 +0200

    chore: Add socials to mobile-nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -22,22 +22,36 @@ const MobileNav = ({ closeNav }: Props) => {
           onClick={closeNav}
         />
       </div>
-      <ul className="mx-8 flex h-2/5 flex-col items-center divide-y divide-slate-700 ">
-        <li className="w-full cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
-          Features
-        </li>
-        <li className="w-full cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
-          Pricing
-        </li>
-        <li className="w-full  cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
-          Contact
-        </li>
-        <li className="w-full py-4 text-center text-xl uppercase text-white">
-          <button className="w-full border-2 py-2 font-medium  uppercase tracking-wider transition duration-300 hover:bg-white hover:text-blue-700">
-            Login
-          </button>
-        </li>
-      </ul>
+      <div className="flex h-4/5 flex-col justify-between">
+        <ul className="mx-8 flex h-2/5 flex-col items-center divide-y divide-slate-700 ">
+          <li className="w-full cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+            Features
+          </li>
+          <li className="w-full cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+            Pricing
+          </li>
+          <li className="w-full  cursor-pointer py-4 text-center text-xl uppercase text-white transition duration-300 hover:text-blue-600">
+            Contact
+          </li>
+          <li className="w-full py-4 text-center text-xl uppercase text-white">
+            <button className="w-full border-2 py-2 font-medium  uppercase tracking-wider transition duration-300 hover:bg-white hover:text-blue-700">
+              Login
+            </button>
+          </li>
+        </ul>
+        <div className="flex items-center justify-center">
+          <img
+            src="/images/icon-facebook.svg"
+            alt="Facebook icon"
+            className="mx-4 w-8 cursor-pointer"
+          />
+          <img
+            src="/images/icon-twitter.svg"
+            alt="Twitter icon"
+            className="mx-4 w-8 cursor-pointer"
+          />
+        </div>
+      </div>
     </nav>
   )
 }
