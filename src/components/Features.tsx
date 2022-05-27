41ef4dee75146cbe04e6d3023bbb65e04ea04762
DonquixoteSabo <mixboksu@gmail.com>
2022-05-27 14:51:57 +0200

    feat: Display features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,10 +1,34 @@
 import React, { useState } from 'react'
 
+const ELEMENTS = ['Simple Bookmarking', 'Speedy Searching', 'Easy Sharing']
+const CONTENT = [
+  {
+    img: '/images/illustration-features-tab-1.svg',
+    title: 'Bookmark in one click',
+    content:
+      'Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites',
+  },
+  {
+    img: '/images/illustration-features-tab-2.svg',
+    title: 'Intelligent search',
+    content:
+      'Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks',
+  },
+  {
+    img: '/images/illustration-features-tab-3.svg',
+    title: 'Share your bookmarks',
+    content:
+      'Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button',
+  },
+]
+
 const Features = () => {
   const [activeEl, setActiveEl] = useState(0)
-  const NAVEL = ['Simple Bookmarking', 'Speedy Searching', 'Easy Sharing']
+
+  const { title, content, img } = CONTENT[activeEl]
+
   return (
-    <section className="mt-32 flex flex-col items-center px-8">
+    <section className="mt-32 flex flex-col items-center px-12">
       <div className="max-w-lg text-center">
         <header>
           <h2 className="text-3xl font-medium text-blue-700">Features</h2>
@@ -15,7 +39,7 @@ const Features = () => {
           them on the go.
         </p>
         <ul className="text-blue-600 md:mt-12 md:flex md:justify-between">
-          {NAVEL.map((el, i) => (
+          {ELEMENTS.map((el, i) => (
             <li
               className={`cursor-pointer border-t border-b py-3 md:border-t-0 ${
                 activeEl === i && 'active-feature'
@@ -27,6 +51,16 @@ const Features = () => {
           ))}
         </ul>
       </div>
+      <article className="container mt-16 px-12  md:flex md:items-center md:justify-between">
+        <img src={img} alt={title} className="w-full object-contain md:w-1/2" />
+        <div className="mt-8 md:w-2/5">
+          <header>
+            <h3 className="text-2xl font-medium text-blue-700">{title}</h3>
+          </header>
+          <p className="my-4 text-blue-600 md:my-8">{content}</p>
+          <button className="primary-button">More info</button>
+        </div>
+      </article>
     </section>
   )
 }
