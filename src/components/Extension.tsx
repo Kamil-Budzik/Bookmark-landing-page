17fa34e33426f2a48be83947ccb7f773ab3b444d
DonquixoteSabo <mixboksu@gmail.com>
2022-05-28 19:15:57 +0200

    feat: Add carts

--- src/components/Extension.tsx
+++ src/components/Extension.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 import Introduction from './Introduction'
+import Cart from './Cart'
+
+const CARTS = [
+  { browser: 'Chrome', version: '62', img: '/images/logo-chrome.svg' },
+  { browser: 'Firefox', version: '55', img: '/images/logo-firefox.svg' },
+  { browser: 'Opera', version: '46', img: '/images/logo-opera.svg' },
+]
 
 const Extension = () => {
   return (
@@ -10,6 +17,11 @@ const Extension = () => {
         content="We've got more browsers in the pipeline. Please do let us know if you've
         got a favourite you'd like us to prioritize."
       />
+      <ul className="flex w-full grid-cols-3 flex-wrap items-center justify-center">
+        {CARTS.map((cart) => (
+          <Cart {...cart} />
+        ))}
+      </ul>
     </section>
   )
 }
