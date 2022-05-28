f049dcb79c7fb96c9664d580c936a416adfca980
DonquixoteSabo <mixboksu@gmail.com>
2022-05-28 18:42:46 +0200

    feat: Add introduction component

---
+++ src/components/Extension.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Introduction from './Introduction'
+
+const Extension = () => {
+  return (
+    <section className="mt-32 flex flex-col items-center px-12">
+      <Introduction
+        title="Dowload the extension"
+        content="We've got more browsers in the pipeline. Please do let us know if you've
+        got a favourite you'd like us to prioritize."
+      />
+    </section>
+  )
+}
+
+export default Extension
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Introduction from './Introduction'
 
 const ELEMENTS = ['Simple Bookmarking', 'Speedy Searching', 'Easy Sharing']
 const CONTENT = [
@@ -30,14 +31,13 @@ const Features = () => {
   return (
     <section className="mt-32 flex flex-col items-center px-12">
       <div className="max-w-lg text-center">
-        <header>
-          <h2 className="text-3xl font-medium text-blue-700">Features</h2>
-        </header>
-        <p className="my-4  text-blue-600">
-          Our aim is to make it quick and easy for you to access your favourite
+        <Introduction
+          title="Features"
+          content=" Our aim is to make it quick and easy for you to access your favourite
           websites. Your bookmarks sync between your devices so you can access
-          them on the go.
-        </p>
+          them on the go."
+        />
+
         <ul className="text-blue-600 md:mt-12 md:flex md:justify-between">
           {ELEMENTS.map((el, i) => (
             <li
